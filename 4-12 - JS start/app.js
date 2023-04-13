1fade35a828f0b46322f8a888053d6bf82e423c6
Leo Curdi <ldcurdi@icloud.com>
2023-04-12 23:53:50 -0700

    notes

--- 4-12 - JS start/app.js
+++ 4-12 - JS start/app.js
@@ -1,9 +1,12 @@
 console.log(undefined == null);
 let total = 4 + 3;
 let age = Math.random() * 30;
-if (age >= 18) {
+if (age > 18) {
     console.log("old. Age: ",age);
 }
-else {
+else if (age < 18) {
     console.log("young. Age: ",age);
+}
+else {
+    console.log("Exactly 18");
 }
