2889a9aabd1dfbae23fbadaf2f8148e4ee355b54
Leo Curdi <ldcurdi@icloud.com>
2023-04-13 00:29:45 -0700

    js logic syntax

--- 4-12 - JS start/app.js
+++ 4-12 - JS start/app.js
@@ -1,6 +1,9 @@
 console.log(undefined == null);
 let total = 4 + 3;
+
 let age = Math.random() * 30;
+//const age = prompt('Enter your age');
+
 if (age > 18) {
     console.log("old. Age: ",age);
 }
@@ -8,6 +11,44 @@ else if (age < 18) {
     console.log("young. Age: ",age);
 }
 else {
-    console.log("Exactly 18");
+    console.log("You're exactly 18");
 }
-console.log(undefined === null);
+
+const password = prompt("please enter a new password");
+
+if (password.length < 6) {
+    console.log("too short");
+}
+else if (password.indexOf(' ')) {
+    console.log("cant use space");
+}
+else {
+    console.log("youre good");
+}
+
+// falsey and truthy
+const userInput = prompt("enter something");
+if (userInput) {
+    console.log("truthy");
+}
+else {
+    console.log("falsey");
+}
+
+
+// switch statement
+const day = 2;
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 6:
+    case 7:
+        console.log("weekend");
+        break;
+    default:
+        console.log("bruh");
+}
